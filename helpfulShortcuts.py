fcfdf196c7f84c54447f7295b15e624e11b54897
Harrison Niswander <113133466+HarrisonNiswander@users.noreply.github.com>
2026-08-27 13:23:12 -0400

    Initial Commit of week 1 stuff

---
+++ helpfulShortcuts.py
@@ -0,0 +1,18 @@
+#Harrison Niswander
+#ECET 114 / ITC 150
+#8-27-26
+#This code will give you helpful shortcuts in python to make your life easier
+
+#--------------------------------------------------------------------------------------------
+
+#How to Comment out a Large Block of Code:
+    #1 Highlight the code you want to comment out
+    #2 Press Ctrl + / (Windows) or Command + / (Mac)
+
+#--------------------------------------------------------------------------------------------
+
+#How to Indent Large Blocks of Code:
+    #1 Highlight the code you want to indent
+    #2 Press Tab to indent or Shift + Tab to un-indent
+
+#--------------------------------------------------------------------------------------------
