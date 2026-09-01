62950d6b9a0f4878012a16e1b6b3744d59ea984c
Harrison Niswander <niswhd01@pfw.edu>
2026-09-01 15:39:14 -0400

    update operations blank notes

--- Input_Processing_Output/Blank/operationsBLANK.py
+++ Input_Processing_Output/Blank/operationsBLANK.py
@@ -1,6 +1,6 @@
-#Harrison Niswander
+#[Name]
 #ECET 114 / ITC 150
-#9-1-26
+#[Date]
 #This program will introduce operations and how to use them (perform math)
 
 #--------------------------------------------------------------------------------------------
