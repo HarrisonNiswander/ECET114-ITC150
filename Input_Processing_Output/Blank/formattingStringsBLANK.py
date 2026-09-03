29bc3097c87c9aa2655ad7abc1e1b83fee0346f9
Harrison Niswander <niswhd01@pfw.edu>
2026-09-03 12:15:00 -0400

    formatting strings blank notes

--- Input_Processing_Output/Blank/formattingStringsBLANK.py
+++ Input_Processing_Output/Blank/formattingStringsBLANK.py
@@ -28,13 +28,12 @@ lengthB = 8
 
 # Calculate the area of the rectangle
 area = lengthA * lengthB
-print(f"The area of the rectangle is {area}.")
 
 # This also works with multiple expressions
-print(f"The area of the rectangle with lengths {lengthA} and {lengthB} is {area}.")
+
 
 # Expressions can also be used directly in the placeholders
-print(f"The area of the rectangle with lengths {10} and {8} is {10 * 8}.")
+
 print()
 
 #--------------------------------------------------------------------------------------------
@@ -52,7 +51,7 @@ print()
 # Example
 value = 100 / 9
 print(f"The value of 100 / 9 not formatted is: {value}")
-print(f"The value of 100 / 9 formatted to 2 decimal places is: {value:.2f}")
+#print(f"The value of 100 / 9 formatted to 2 decimal places is: {}")
 print()
 
 #--------------------------------------------------------------------------------------------
@@ -63,10 +62,9 @@ print()
 # Example
 number = 1000000
 print(f"The number not formatted is: {number}")
-print(f"The number formatted with commas is: {number:,}")
-print(f"The number formatted with underscores is: {number:_}")
+#print(f"The number formatted with commas is: {}")
+#print(f"The number formatted with underscores is: {}")
 print()
-# print(f"The number formatted with commas is: {number / 3:,.2f}")
 
 #--------------------------------------------------------------------------------------------
 
@@ -76,12 +74,12 @@ print()
 # Example
 number2 = 0.75
 print(f"The percentage not formatted is: {number2}")
-print(f"The percentage formatted is: {number2:%}")
+#print(f"The percentage formatted is: {}")
 
 # Example 2
 decimalPercent = 0.234237
-print(f"Percent: {decimalPercent:%}")
-print(f"Percent (2 decimal places): {decimalPercent:.2%}")
+#print(f"Percent: {}")
+#print(f"Percent (2 decimal places): {}")
 print()
 
 #--------------------------------------------------------------------------------------------
@@ -92,7 +90,7 @@ print()
 # Example
 number3 = 123456789
 print(f"The number not formatted is: {number3}")
-print(f"The number formatted in scientific notation is: {number3:e}")
+#print(f"The number formatted in scientific notation is: {}")
 print()
 
 #--------------------------------------------------------------------------------------------
@@ -103,7 +101,7 @@ print()
 # Example
 number4 = 1234567890
 print(f"The number not formatted is: \t\t{number4}")
-print(f"The number formatted as an integer is: \t{number4:,d}")
+#print(f"The number formatted as an integer is: \t{}")
 print()
 
 #--------------------------------------------------------------------------------------------
@@ -114,7 +112,7 @@ print()
 # Example
 number5 = 123
 print(f"The number not formatted is: {number5}")
-print(f"The number is 10 spaces away: {number5:10}")
+#print(f"The number is 10 spaces away: {}")
 print()
 
 #--------------------------------------------------------------------------------------------
@@ -128,21 +126,17 @@ print()
 text1 = "Indiana"
 text2 = "Ohio"
 text3 = "Michigan"
-print(f"{text1:>10}")
-print(f"{text2:>10}")
-print(f"{text3:>10}")
+#print(f"{}")
+#print(f"{}")
+#print(f"{}")
 print()
-
-# When showing example start out with not putting number in
-# Then put in 5 - States are right aligned at index 5 (spaces)
-# Finally, put in 10 - States are right aligned at index 10 (spaces)
 
 #--------------------------------------------------------------------------------------------
 
 # F-String Formatters Can be Combined Together
 number6 = 1567.891011
 print(f"The number not formatted is: {number6}")
-print(f"Formatted: {number6:,.2f}")
+#print(f"Formatted: {}")
 print()
 
 # Order in Which F-String Formatters are Applied
@@ -163,18 +157,10 @@ print()
 print("This is a really long string that should be broken up into multiple lines for better readability in a Python program. Even though it is not syntactically incorrect to have a long string on one line, it is good practice to break it up into multiple lines for better readability.\n")
 
 # More Readable Version of the Above Code
-print("This is a really long string that should be broken up into multiple lines for better readability in a Python program. " +
-    "Even though it is not syntactically incorrect to have a long string on one line, it is good practice to break it up into " +
-    "multiple lines for better readability.\n")
+
 
 # Even Better - Print on Multiple Lines
-print("This is a really long string that should be broken up into multiple lines for better readability in a Python program. \n" +
-    "Even though it is not syntactically incorrect to have a long string on one line, it is good practice to break it up into \n" +
-    "multiple lines for better readability.\n")
 
-# Both Print Statements Output the Same Result, but the Second Version is More Readable
-
-# IMPORTANT - Code should NOT only work but be readable!!!
 
 #--------------------------------------------------------------------------------------------
 
@@ -183,16 +169,12 @@ title = "Mr."
 firstName = "Harrison"
 lastName = "Niswander"
 
-print(f"{title} {firstName} {lastName}")
-print(f"{title}" + f"{firstName}" + f"{lastName}")
-#print(f"{title} " + f"{firstName} " + f"{lastName}")
-
 
 # Concatenation With Numbers
 percent = .90242
 largeNum = 7283742
 decimal = 56.823472
-print(f"These numbers are all formatted: {percent:.2%} --- " + f"{largeNum:,} --- " + f"{decimal:.4f}")
+#print(f"These numbers are all formatted: ")
 print()
 
 
